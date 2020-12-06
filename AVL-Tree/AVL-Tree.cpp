c4ce0ce9f73e9ce1669b1cc4aa3dc5b72dc0c46c
123 <123@MIXAL>
2020-12-06 18:40:05 +0300

    upd

--- AVL-Tree/AVL-Tree.cpp
+++ AVL-Tree/AVL-Tree.cpp
@@ -1,17 +1,22 @@
-﻿#include <iostream>
-#include <string>
-
-template <class T>
+﻿template <class T>
 class avl_node
 {
-    public:
 
-    private: 
+public:
+    
+        int get_height()
+        {
+            if (root == NULL) return 0;
+            return root.height;
+        };
 
-    T value;
+    T data;
     avl_node* left;
     avl_node* right;
-        
+
+private:
+
+    int height;
 };
 
 template <class T>
@@ -21,47 +26,56 @@ class avl_tree
         //получить корень дерева
         avl_tree<t> const get_root();
 
+        //вставка
         bool insert(const T& data);
 
+        //удаление
         bool remove(const T& data);
 
-        avl_node<T>* find(avl_node<T>* root, const T& value) const;
-
-        //Высота узла
-        int height(avl_node<t>* subtree) const;
-
-        //Посчитать баланс-фактор узла дерева
-        int balance_factor(avl_node<t>* subtree) const;
-
-        
+        //поиск
+        avl_node<T>* find(avl_node<T>* root, const T& data) const;
 
     private: 
         
+        //Баланс-фактор поддерева
+        int balance_factor(avl_node<t>* root) const
+        {
+            if (root == NULL) {
+                return 0;
+            }
+            return root->left->get_height- root->right->get_height;
+        };
 
         //Повороты дерева
+        
+        //LL
         void left_rotate(avl_node<T>& root) noexcept
         {
-            avl_node<T> newroot = root.right;
-            newroot.right = root.right->right;
-            root.right = root.right.left;
-            newroot.left = root;
+            avl_node<T> newroot = root->right;
+            newroot->right = root->right->right;
+            root->right = root->right->left;
+            newroot->left = root;
             root = newroot;
         };
 
+        //RR
         void right_rotate(avl_node<T>& root) noexcept
         {
-            avl_node<T> newroot = root.left;
-            newroot.left = root;
-            root.left = root.left.right;
-            newroot.right = root;
+            avl_node<T> newroot = root->left;
+            newroot->left = root;
+            root->left = root->left->right;
+            newroot->right = root;
             root = newroot;
         };
 
+        //LR
         void left_right_rotate(avl_node<T>& root) noexcept
         {
             left_rotate(root.left);
             right_rotate(root);
         };
+
+        //RL
         void right_left_rotate(avl_node<T>& root) noexcept
         {
             right_rotate(root.right);
@@ -71,7 +85,3 @@ class avl_tree
         
 };
 
-int main()
-{
-    return 0;
-};
